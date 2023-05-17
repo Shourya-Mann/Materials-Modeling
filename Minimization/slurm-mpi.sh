1f9247011afb29a520dc071f08fd847594069985
Shourya Vir Singh Mann <132126960+Shourya-Mann@users.noreply.github.com>
2023-05-17 16:23:12 -0400

    Add files via upload

---
+++ Minimization/slurm-mpi.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#NOTE the -1 flag!
+
+# Name of the Job
+#SBATCH -J STO
+
+#Standard out and Standard error output files
+#SBATCH -o STO.output
+#SBATCH -e STO.output
+
+#Sends and email (must remove a #)
+##SBATCH --mail-user sm4836@rit.edu
+
+##Notify on state change: BEGIN, END, FAIL or ALL
+##SBATCH --mail-type=ALL
+
+#Request 24 hours run time MAX, anything over will be KLLED
+#SBATCH -t 12:0:0
+
+#Put the job in the work partition and request FOUR cores
+#SBATCH -p tier3 -A hea -n 4
+
+#Keep everything on one node because lammps has a meltdown otherwise
+#SBATCH --nodes=1
+
+#job memory requirements in MB
+#SBATCH --mem=1000
+
+#The job script goes now
+
+# The job script goes now
+module load lammps-20181212-gcc-8.2.0-d7ufswbn
+orterun -np $SLURM_NTASKS lmp -in in.STO
+
